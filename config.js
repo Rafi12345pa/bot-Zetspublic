2c76fb361c14a1359d0678aad8861bd03474ebfc
MuhammadRafi <108687316+Rafi12345pa@users.noreply.github.com>
2022-07-24 19:00:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -40,10 +40,10 @@ global.limitawal = {
     premium: "Infinity",
     free: 300
 }
-global.sc = fs.readFileSync('./media/sc.jpg')
+global.sc = fs.readFileSync('./media/xavi.jpg')
 global.tq = fs.readFileSync('./media/tq.jpg')
-global.menu = fs.readFileSync('./media/menu.jpg')
-global.allmenu = fs.readFileSync('./media/allmenu.jpg')
+global.menu = fs.readFileSync('./media/xavi.jpg')
+global.allmenu = fs.readFileSync('./media/xavi.jpg')
 global.thumb = { url: 'https://i.pinimg.com/736x/d0/f8/b8/d0f8b804a908ce4aaee63d54035d2192.jpg' }
 global.visoka = { url: 'https://telegra.ph/file/769d88bb8c8357546a149.mp4' }
 
